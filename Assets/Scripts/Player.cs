3f88bd8aad811ccd7fc28332a0c704ec8235d975
Matthew Pomes <the10thwiz@gmail.com>
2018-11-07 16:46:17 -0600

    Adds Player Logic
    Players do not slide down, but do stop when hitting the ground, or a
    wall. Later, sliding will be added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,6 +6,7 @@ public class Player : MonoBehaviour {
 
 	public float gravity = -20;
 	public float moveSpeed = 6;
+	public float jumpSpeed = 8;
 	Vector3 velocity;
 
 	PhysicsController controller;
@@ -17,9 +18,16 @@ public class Player : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		if(controller.collisions.above || controller.collisions.below) {
+			velocity.y = 0;
+		}
+
 		Vector2 input = new Vector2 (Input.GetAxisRaw ("Horizontal"), Input.GetAxisRaw ("Vertical"));
 
 		velocity.x = input.x * moveSpeed;
+		if(controller.collisions.below && input.y > 0) {
+			velocity.y = input.y * jumpSpeed;
+		}
 
 		velocity.y += gravity * Time.deltaTime;
 		controller.Move (velocity * Time.deltaTime);
