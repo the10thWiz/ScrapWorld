c492aa55451ddeefdd4f46f281bff119be4185a9
Matthew Pomes <the10thwiz@gmail.com>
2018-11-11 17:08:27 +0000

    Add Weapon System
    Also adds MeleeWeapon, a ranged instant attack

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour {
+	public float attack_time = 0.1f;
+	
+	float lastAttack = -10;
+	protected bool CanAttack() {
+		return Time.time > lastAttack + attack_time;
+	}
+	protected void Attack() {
+		lastAttack = Time.time;
+	}
+
+	public virtual void Attack(Vector3 dir) {
+	}
+
+}
