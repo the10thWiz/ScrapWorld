af148f5948d4e9ba9828457bff5722eee37d336c
Matthew Pomes <the10thwiz@gmail.com>
2018-11-11 17:07:44 +0000

    Add Health System

---
+++ Assets/Scripts/Armour.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Health))]
+public class Armour : MonoBehaviour {
+
+	public float effectiveness = 50;
+	public float durablity = 100;
+
+	void Start() {
+		GetComponent<Health>().ApplyArmour(effectiveness);
+	}
+
+	public void Damage(float damage) {
+		durablity-= damage;
+	}
+	
+	public bool IsBroken() {
+		return durablity <= 0;
+	}
+}
