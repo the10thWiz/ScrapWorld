1d41d7da0f35cf6f7a14278138ac74190086b4bd
Matthew Pomes <the10thwiz@gmail.com>
2018-11-11 17:09:35 +0000

    Add attack button

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour {
 	public float jumpSpeed = 8;
 	public float acceleration = .2f;
 	Vector3 velocity;
+	Vector3 dir;
 	float velocityXSmoothing;
 
 	PhysicsController controller;
@@ -20,6 +21,11 @@ public class PlayerController : MonoBehaviour {
 
 	// Update is called once per frame
 	void Update () {
+		Move();
+		Attack();
+	}
+
+	void Move() {
 		if (controller.collisions.above || controller.collisions.below) {
 			velocity.y = 0;
 		}
@@ -33,5 +39,17 @@ public class PlayerController : MonoBehaviour {
 		velocity.x = Mathf.SmoothDamp (velocity.x, targetX, ref velocityXSmoothing, (controller.collisions.below) ? acceleration / 2 : acceleration);
 		velocity.y += gravity * Time.deltaTime;
 		controller.Move (velocity * Time.deltaTime);
+		dir.y = 0;
+		dir.x = Mathf.Sign(velocity.x);
+	}
+
+	void Attack() {
+		if(Input.GetMouseButton(0)) {
+			Weapon w = GetComponent<Weapon>();
+			if(w) {
+				Debug.Log("Attacking");
+				w.Attack(Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position);
+			}
+		}
 	}
 }
